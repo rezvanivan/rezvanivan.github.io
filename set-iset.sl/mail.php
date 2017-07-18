5e37a0499ac9886377813e8b1d8ac275d9e7d708
ivan <rezvan_ivan@mail.ru>
2017-07-18 10:58:41 +0300

    init

---
+++ set-iset.sl/mail.php
@@ -0,0 +1,29 @@
+<?php
+
+	//Принимаем постовые данные
+	$whatever=$_POST['whatever'];
+	$username=$_POST['username'];
+	$email=$_POST['email'];
+	$user_message=$_POST['user_message'];
+
+	//Тут указываем на какой ящик посылать письмо
+	$to = "navzer@mail.ru";
+
+	//Далее идет тема письма и само сообщение
+	// Тема письма
+	$subject = "Заявка с сайта sd";
+
+	// Сообщение письма
+	$message = "
+	Форма, которую заполнил клиент: ".htmlspecialchars($whatever)."<br />
+	Имя пользователя: ".htmlspecialchars($username)."<br />
+	Email: ".htmlspecialchars($email)."<br />
+	Сообщение: ".htmlspecialchars($user_message);
+
+	// Отправляем письмо при помощи функции mail();
+	$headers = "From: sd.sl <putin@homework.sl>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+	// Отправляем письмо при помощи функции mail();
+	mail ($to, $subject, $message, $headers);
+	exit();
+
+?>
